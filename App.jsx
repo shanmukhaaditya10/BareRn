c535b4b45620a86a6062ed50d5c5fbfcab1a6b64
shanmukhaaditya10 <adityaofficial1022004@gmail.com>
2023-11-04 23:57:38 +0530

    RNcamera

--- App.jsx
+++ App.jsx
@@ -1,14 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { RNCamera ,} from 'react-native-camera';
 
 const App = () => {
-  return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
-}
+ const [faces, setFaces] = useState([]);
 
-export default App
+ return (
+   <View style={styles.container}>
+     <RNCamera
+       style={styles.camera}
+       captureAudio={false}
+       onFacesDetected={(faces) => {
+         setFaces(faces.faces);
+       }}
+       type={RNCamera.Constants.Type.front}
+       flashMode={RNCamera.Constants.FlashMode.on}
+     />
+     {faces.map((face, index) => (
+       <View
+         key={index}
+         style={{
+          position: 'absolute',
+          left: face.bounds.origin.x,
+          top: face.bounds.origin.y,
+          width: face.bounds.size.width,
+          height: face.bounds.size.height,
+          borderWidth: 2,
+          borderColor: 'red',
+        }}
+       />
+     ))}
+   </View>
+ );
+};
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+ container: {
+   flex: 1,
+ },
+ camera: {
+   flex: 1,
+ },
+});
+
+export default App;
